d2e598d9039df91cdfe5a05b99f2729438b6d0fd
Jacob Odero <jackjax617@gmail.com>
2020-12-20 14:33:21 +0300

    finishing touches

--- details.php
+++ details.php
@@ -17,22 +17,25 @@
 <?php $title = $blog['title'] ?>
 <?php include 'templates/header.php' ?>
 
-    <?php if($blog){ ?>
-        <div class="blog-header">
-            <h3><?php echo $blog['title'] ?> | <small>By: <?php echo $blog['email'] ?></small></h3>
-        </div>
-        <div class="blog-content">
-            <div class="the-blog">
-                <div class="the-blog-img">
-                    <img src="<?php echo $blog['illustration'] ?>">
-                    <p><small>Created on <?php echo $blog['created_on'] ?></small></p>
-                </div>
-                <div class="the-blog-content">
-                    <p> <?php echo $blog['content'] ?> </p>
+    <div class="a-blog">
+        <?php if($blog){ ?>
+            <div class="blog-header">
+                <h3><?php echo $blog['title'] ?> | <small>By: <?php echo $blog['email'] ?></small></h3>
+            </div>
+            <div class="blog-content">
+                <div class="the-blog">
+                    <div class="the-blog-img">
+                        <img src="<?php echo $blog['illustration'] ?>">
+                        <p><small>Created on <?php echo $blog['created_on'] ?></small></p>
+                    </div>
+                    <div class="the-blog-content">
+                        <p> <?php echo $blog['content'] ?> </p>
+                    </div>
                 </div>
             </div>
-        </div>
-    <?php }?>
+        <?php }?>
+    </div>
+
     <?php include 'templates/footer.php' ?>
 
 </body>
