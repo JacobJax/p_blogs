baf2c654acbd7040db468fc65c1ed45bdf394026
Jacob Odero <jackjax617@gmail.com>
2020-12-20 13:09:34 +0300

    change server

--- config/db_config.php
+++ config/db_config.php
@@ -1,6 +1,10 @@
 <?php
-    // connect to the database
-	$conn = mysqli_connect('localhost', 'yoshi', 'esting123', 'p_blog');
+	// connect to the database
+	#dev server
+	// $conn = mysqli_connect('localhost', 'yoshi', 'esting123', 'p_blog');
+
+	#production server
+	$conn = mysqli_connect('remotemysql.com', 'yG0UYUTkhb', 'J3hzbxcoVA', 'yG0UYUTkhb');
 
 	// check connection
 	if(!$conn){
