a60fb180c995b2d9026e93c5a96b8e27c9c62e27
Jacob Odero <jackjax617@gmail.com>
2020-12-20 14:04:39 +0300

    change headers

--- add.php
+++ add.php
@@ -96,7 +96,7 @@
 			<br><textarea name="content" id="" cols="30" rows="10" style="height: 90px;"></textarea><br>
 			<div class="red-text"><?php echo $errors['content']; ?></div><br>
             <label>Add image illustration</label>
-            <input class='btn' type="file" name="file" value="Upload"><br>
+            <input class='btn' type="file" name="file" value="Upload" style="width: 200px;"><br>
 			<br><div class="center">
 				<input type="submit" name="submit" value="Submit" class="btn brand z-depth-0">
 			</div>
